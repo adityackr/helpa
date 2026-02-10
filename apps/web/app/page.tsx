2abfdce95f7c6ca2c660058f3fc6f2778787bbbb
adityackr <aditya09me@gmail.com>
2026-02-10 18:40:29 +0600

    feat: Implement Clerk authentication for the web application and integrate with Convex.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,14 @@
 'use client';
 
+import { SignInButton, UserButton } from '@clerk/nextjs';
 import { api } from '@workspace/backend/_generated/api';
 import { Button } from '@workspace/ui/components/button';
-import { useMutation, useQuery } from 'convex/react';
+import {
+	Authenticated,
+	Unauthenticated,
+	useMutation,
+	useQuery,
+} from 'convex/react';
 
 export default function Page() {
 	const users = useQuery(api.users.getMany);
@@ -10,18 +16,31 @@ export default function Page() {
 	const addUser = useMutation(api.users.add);
 
 	return (
-		<div className="flex items-center justify-center min-h-svh">
-			<div className="flex flex-col gap-4 items-center justify-center">
-				<p>apps/web</p>
-				<Button onClick={() => addUser({})}>Add User</Button>
-				<div className="max-w-sm w-full mx-auto">
-					<ul>
-						{users?.map((user) => (
-							<li key={user._id}>{user.name}</li>
-						))}
-					</ul>
+		<>
+			<Authenticated>
+				<div className="flex items-center justify-center min-h-svh">
+					<div className="flex flex-col gap-4 items-center justify-center">
+						<p>apps/web</p>
+						<Button onClick={() => addUser({})}>Add User</Button>
+						<UserButton />
+						<div className="max-w-sm w-full mx-auto">
+							<ul>
+								{users?.map((user) => (
+									<li key={user._id}>{user.name}</li>
+								))}
+							</ul>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			</Authenticated>
+			<Unauthenticated>
+				<div className="flex items-center justify-center min-h-svh">
+					<div className="flex flex-col gap-4 items-center justify-center">
+						<p>apps/web</p>
+					</div>
+					<SignInButton />
+				</div>
+			</Unauthenticated>
+		</>
 	);
 }
